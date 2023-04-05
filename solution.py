c53f911188a23f1feded7b6b874cd570e3f9e3e1
Daniil <106452573+DEli-26@users.noreply.github.com>
2023-04-05 20:49:27 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,4 +8,5 @@ def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    dev = [i + 1 for i in range(len(x))]
+    return (x / dev).mean()
