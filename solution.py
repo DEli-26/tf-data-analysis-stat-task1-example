46349ef34dcb60027cf59cc096e3b1f9a1efcada
Daniil <106452573+DEli-26@users.noreply.github.com>
2023-04-06 22:33:32 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,16 +8,5 @@ def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    dev = [i + 1 for i in range(len(x))]
-    return (x / dev).mean()
-    
-    # Определение функции правдоподобия модели
-    #def likelihood_func(mu: float, x: np.array) -> float:
-    #    n = len(x)
-    #    return -n * np.log(mu) - np.sum(x) / mu
+    return x.mean() / 28 
 
-    # Максимизация функции правдоподобия
-    #res = minimize(likelihood_func, x0=1, args=(x,), method='Nelder-Mead')
-    
-    # Возвращение оценки параметра модели
-    #return res.x[0]
